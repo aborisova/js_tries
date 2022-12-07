15b3a9ba0a0be0c3c4fe842f38155f0851ba3955
aborisova <borisova.albina@gmail.com>
2022-12-07 21:51:18 +0300

    Files of practice added

---
+++ practice1.js
@@ -0,0 +1,15 @@
+{
+	function a(){
+		return "b";
+	}
+	a.c = "lslsls";
+	console.log (a.c); 
+	console.log (a()); 
+	console.log (a("ff")); 
+}
+{
+	var ar = [1, "rr", true];
+	ar.h = "ggggg";
+	console.log(ar[2]); 
+	//console.log(ar.1); 
+}
